0a7d4640616a2019f80a37902fb0a185291f9bc9
apardyl <adampa14@gmail.com>
2015-04-16 19:00:03 +0200

    Add mission phase and server status, Fix logo

--- js/tswebapp.js
+++ js/tswebapp.js
@@ -6,5 +6,32 @@ $(document).ready(function() {
     })
         .sidebar('attach events', '.view-sidebar', 'toggle')
     ;
-
 });
+
+function setMissionPhase(phase) { // phase: 1 - Launch preparation, 2 - Launch, 3 - Descend, 4 - Ground operations, 5 - Mission complete
+    "use strict";
+    var i = 1;
+    $('#mission-phase > div').each(function() {
+        if(i < phase) {
+            $(this).removeClass("disabled active");
+        }
+        else if(i === phase) {
+            $(this).addClass("active").removeClass("disabled");
+        }
+        else {
+            $(this).removeClass("active").addClass("disabled");
+        }
+        i++;
+    });
+}
+
+function setSystemStatus(status) { // status: 0 - Connecting, -1 - Connection failed, 1 - Connected
+    "use strict";
+    if (status === 0) {
+        $('#system-status').html("Connecting").css("color", "black").next().addClass("active");
+    } else if (status === 1) {
+        $('#system-status').html("Connected").css("color", "green").next().removeClass("active");
+    } else if (status === -1) {
+        $('#system-status').html("Connection failed!").css("color", "red").next().removeClass("active");;
+    }
+}

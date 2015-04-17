f3bacd159a3202d1c4fae53d56c436c50a2775fe
apardyl <adam@pardyl.com>
2015-04-17 14:12:38 +0200

    Status bar improvements

--- js/tswebapp.js
+++ js/tswebapp.js
@@ -4,14 +4,13 @@ $(document).ready(function() {
         transition       : 'overlay',
         mobileTransition : 'overlay'
     })
-        .sidebar('attach events', '.view-sidebar', 'toggle')
-    ;
+        .sidebar('attach events', '.view-sidebar', 'toggle');
 });
 
 function setMissionPhase(phase) { // phase: 1 - Launch preparation, 2 - Launch, 3 - Descend, 4 - Ground operations, 5 - Mission complete
     "use strict";
     var i = 1;
-    $('#mission-phase > div').each(function() {
+    $('#mission-phase').find('> div').each(function() {
         if(i < phase) {
             $(this).removeClass("disabled active");
         }
@@ -25,13 +24,26 @@ function setMissionPhase(phase) { // phase: 1 - Launch preparation, 2 - Launch, 
     });
 }
 
-function setSystemStatus(status) { // status: 0 - Connecting, -1 - Connection failed, 1 - Connected
+function setServerStatus(status) { // status: 0 - Connecting, -1 - Connection failed, 1 - Connected
     "use strict";
     if (status === 0) {
-        $('#system-status').html("Connecting").css("color", "black").next().addClass("active");
+        $('#status-server').html("Connecting").css("color", "black").parent().next().find('.loader').addClass("active");
+        $('#status-cansat').html(" – ").css("color", "black");
     } else if (status === 1) {
-        $('#system-status').html("Connected").css("color", "green").next().removeClass("active");
+        $('#status-server').html("Connected").css("color", "green").parent().next().find('.loader').removeClass("active");
     } else if (status === -1) {
-        $('#system-status').html("Connection failed!").css("color", "red").next().removeClass("active");;
+        $('#status-server').html("Connection failed!").css("color", "red").parent().next().find('.loader').removeClass("active");
+        $('#status-cansat').html(" – ").css("color", "black");
+    }
+}
+
+function setCansatStatus(status) { // status: 0 - Unknown , -1 - Offline, 1 - Online
+    "use strict";
+    if (status === 0) {
+        $('#status-cansat').html(" – ").css("color", "black");
+    } else if (status === 1) {
+        $('#status-cansat').html("Online").css("color", "green");
+    } else if (status === -1) {
+        $('#status-cansat').html("Offline").css("color", "black");
     }
 }
